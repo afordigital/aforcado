60aa17af46b0a81ff5ebbf1c7f7dc465eb389ee3
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-01-31 23:56:22 +0100

    feat: display letters

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
-import Tmi from 'tmi.js'
+import JSConfetti from 'js-confetti'
 import { GAME_STATE, BLACK_USERS, WORDS } from './constants'
+import Tmi from 'tmi.js'
 
 const client = new Tmi.Client({
   options: { debug: false },
@@ -8,55 +9,94 @@ const client = new Tmi.Client({
 })
 
 const chooseWord = () => {
-  let index = Math.floor(Math.random() * WORDS.length)
-  let word = WORDS[index]
+  const index = Math.floor(Math.random() * WORDS.length)
+  const word = WORDS[index]
 
-  return word
+  return word.toUpperCase()
 }
+
+const USER_LIFES = 6
+const jsConfetti = new JSConfetti()
 
 function App () {
   const [state, setState] = useState<string>(GAME_STATE.MENU)
+  const [bannedUsers, setBannedUsers] = useState<string[]>(BLACK_USERS)
   const [writtenLetters, setWrittenLetters] = useState<string[]>([])
   const [chosenWord, setChosenWord] = useState<string>(chooseWord)
+  const [lifes, setLifes] = useState<number>(USER_LIFES)
+
+  const hasWon = chosenWord
+    .split('')
+    .every(letter => writtenLetters.includes(letter))
+
+  const hasLost = lifes === 0
 
   const addWrittenLetter = (letter: string) => {
     setWrittenLetters(prev => [...prev, letter])
   }
 
-  console.log(chosenWord)
-
   useEffect(() => {
-    client.on('message', async (channel, userState, message) => {
-      const { username, ['display-name']: displayName } = userState
-
-      if (!username || BLACK_USERS.includes(username)) {
-        return
-      }
-
-      if (message.startsWith('!play ')) {
-        const letter = message.slice(6, 7)
-        const regex = /^[a-zA-Z]$/
-
-        if (letter.match(regex) && !writtenLetters.includes(letter)) {
-        }
-      }
-
-      console.log(userState)
-    })
     client.connect()
 
     return () => {
-      client.removeAllListeners('message')
       client.disconnect()
     }
   }, [])
+
+  useEffect(() => {
+    client.on('message', async (channel, userState, message) => {
+      const { username } = userState
+
+      if (!username || bannedUsers.includes(username)) {
+        return
+      }
+
+      if (message.startsWith('!play ') && !hasWon) {
+        const letter = message.slice(6, 7).toUpperCase()
+        const regex = /^[a-zA-ZñÑ]$/
+
+        if (letter.match(regex) && !writtenLetters.includes(letter)) {
+          addWrittenLetter(letter)
+          validateLetter(letter, username)
+        }
+      }
+    })
+
+    return () => {
+      client.removeAllListeners('message')
+    }
+  }, [writtenLetters, bannedUsers, hasWon])
 
   const updateGame = (state: string) => {
     setState(state)
   }
 
+  useEffect(() => {
+    if (hasWon) {
+      jsConfetti.addConfetti({
+        emojis: ['🐹', '🐀', '🐁', '🐭'],
+        confettiNumber: 200
+      })
+    }
+
+    if (hasLost) {
+      updateGame(GAME_STATE.MENU)
+      setChosenWord(chooseWord)
+      setWrittenLetters([])
+      setLifes(USER_LIFES)
+      setBannedUsers(BLACK_USERS)
+    }
+  }, [hasWon, hasLost])
+
+  const validateLetter = (letter: string, username: string) => {
+    if (!chosenWord.includes(letter)) {
+      setLifes(lifes => lifes - 1)
+      setBannedUsers(users => [...users, username])
+    }
+  }
+
   return (
-    <main className='w-screen min-h-screen flex flex-col py-20 text-white bg-[url(/imgs/bg.jpeg)] bg-cover font-inika'>
+    <main className='w-screen min-h-screen h-screen flex flex-col py-20 text-white bg-[url(/imgs/bg.jpeg)] bg-cover font-inika'>
       {state === GAME_STATE.MENU ? (
         <>
           <img src='/imgs/title.png' alt='aforcado-title' />
@@ -73,7 +113,7 @@ function App () {
           </div>
         </>
       ) : (
-        <div className='pl-20'>
+        <div className='pl-20 h-full flex flex-col justify-center'>
           <div className='flex gap-x-8'>
             <button
               onClick={() => updateGame(GAME_STATE.MENU)}
@@ -81,14 +121,57 @@ function App () {
             >
               MENU
             </button>
-            <button className='bg-[#D30040] hover:bg-[#F10049] w-fit text-xl px-[20px] py-[4px] text-white border-2 border-black font-bold rounded-[8px]'>
+            <button
+              onClick={() => {
+                setLifes(0)
+              }}
+              className='bg-[#D30040] hover:bg-[#F10049] w-fit text-xl px-[20px] py-[4px] text-white border-2 border-black font-bold rounded-[8px]'
+            >
               TRY AGAIN
             </button>
           </div>
 
-          <div></div>
+          <div className='flex-1 h-full flex gap-8 items-center'>
+            {chosenWord.split('').map((letter, index) => (
+              <div key={index} className='w-[100px] flex flex-col'>
+                <span className='w-[100px] text-black text-center font-bold border-b-[16px] border-black text-[64px] capitalize'>
+                  {writtenLetters.includes(letter) ? letter : '-'}
+                </span>
+              </div>
+            ))}
+          </div>
 
-          <div className='w-[600px] h-[600px] absolute right-0 top-0 bottom-0 bg-[url(/imgs/step-0.png)]'></div>
+          <div className='grid grid-cols-8 gap-y-4'>
+            {writtenLetters.map(letter => (
+              <>
+                {!chosenWord.includes(letter) && (
+                  <div className='flex flex-col'>
+                    <span
+                      key={letter}
+                      className='w-[75px] h-[75px] font-bold border-2 border-black rounded-md text-4xl bg-[#FF9C54] text-black flex items-center justify-center'
+                    >
+                      {letter}
+                    </span>
+                  </div>
+                )}
+              </>
+            ))}
+          </div>
+
+          <div className='flex gap-x-6'>
+            {bannedUsers.map(user => (
+              <span className='px-8 py-2 text-black border-2 rounded-md bg-[#FFEE54]'>
+                {user}
+              </span>
+            ))}
+          </div>
+
+          <div
+            style={{
+              background: `url('imgs/step-${USER_LIFES - lifes}.png')`
+            }}
+            className='w-[600px] h-[600px] absolute right-0 top-0 bottom-0'
+          ></div>
         </div>
       )}
     </main>
--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-export const BLACK_USERS = ['nightbot']
+export const BLACK_USERS = []
 
 export const GAME_STATE = {
   MENU: 'MENU',
   GAME: 'GAME'
 }
 
-export const WORDS = ['Afordin', 'Hola', 'Suscríbete']
+export const WORDS = ['cerveza']
